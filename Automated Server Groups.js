525c1811e75ca97cb30c0f6dc9c48ebec5c63906
Relentless <damnrelentless.yt@gmail.com>
2019-01-26 02:50:20 +0100

    Added oklib version check to prevent errors

--- Automated Server Groups.js
+++ Automated Server Groups.js
@@ -411,11 +411,14 @@ function (SinusBot, config) {
       engine.log('[RLNT] ASG > OKlib wasn\'t found!')
       engine.log('[RLNT] ASG > Install the OKlib or the script is disabled!')
       engine.log('[RLNT] ASG > OKlib can be downloaded here: https://forum.sinusbot.com/resources/oklib.325/')
-      return
-    }
-
+    } else if (!oklib.general.checkVersion('1.0.7')) {
+      engine.log('[RLNT] ASG > OKlib is outdated!')
+      engine.log('[RLNT] ASG > Minimum version of OKlib is 1.0.7!')
+      engine.log('[RLNT] ASG > OKlib can be downloaded here: https://forum.sinusbot.com/resources/oklib.325/')
+    } else {
     /* EXECUTE MAIN FUNCTION */
     RLNT_ASG(oklib)
+    }
   })
 
   /* MAIN FUNCTION */
