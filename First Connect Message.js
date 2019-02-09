1f42a832dfb0a6ddc69b55f524c69e4f07fd7693
Relentless <damnrelentless.yt@gmail.com>
2019-02-09 02:57:22 +0100

    Bumped version of FCM

--- First Connect Message.js
+++ First Connect Message.js
@@ -12,7 +12,7 @@ registerPlugin({
   name: 'First Connect Message',
   author: 'RLNT <RLNT@damn-community.com>',
   description: 'With this script, the bot will automatically message a user when joining for the first time.',
-  version: '1.0.0',
+  version: '1.0.1',
   backends: ['ts3'],
   vars: [
     {
